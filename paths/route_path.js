322cdc1246be6a647203f71f4cb23c95861637f0
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2022-04-30 14:01:23 +0600

    updated

--- paths/route_path.js
+++ paths/route_path.js
@@ -3,9 +3,7 @@ const all_paths = [
     require('../api/routes/upload'),
     require('../api/routes/signup-login/signup-login'),
     require('../api/routes/user/user'),
-    require('../api/routes/user/user_role'),
-    require('../api/routes/vehicle/tow_category'),
-    require('../api/routes/vehicle/vehicle')
+    require('../api/routes/user/user_role')
 ];
 
 module.exports = all_paths;
